dd12391d348395b28398417821741313db62336f
shenyihong <shen_yi_hong@outlook.com>
2022-09-17 18:51:44 +0800

    Fix `checkstyle` violations

--- src/main/java/seedu/address/model/person/Remark.java
+++ src/main/java/seedu/address/model/person/Remark.java
@@ -9,6 +9,11 @@ import static java.util.Objects.requireNonNull;
 public class Remark {
     public final String value;
 
+    /**
+     * Constructor for Remark class.
+     *
+     * @param remark a string representing a remark
+     */
     public Remark(String remark) {
         requireNonNull(remark);
         value = remark;
